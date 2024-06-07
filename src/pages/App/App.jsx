d97d6efb50582fd43759fc1be386a9b733fbd8df
Andrea Macias <andreamacrz@gmail.com>
2024-06-06 20:33:34 -0400

    setup

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,11 @@
+import Projects from "../Projects/Projects";
+import NavBar from "../../components/NavBar/NavBar";
+
+export default function App() {
+  return (
+    <>
+      <NavBar />
+      <Projects />
+    </>
+  );
+}
