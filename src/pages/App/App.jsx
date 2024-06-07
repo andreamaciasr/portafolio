11defa79c478e56b1ba8fb78817a85fcef021177
Andrea Macias <andreamacrz@gmail.com>
2024-06-06 22:25:25 -0400

    carousel working, wip

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,5 +1,9 @@
+// import function to register Swiper custom elements
+import { register } from "swiper/element/bundle";
 import Projects from "../Projects/Projects";
 import NavBar from "../../components/NavBar/NavBar";
+
+register();
 
 export default function App() {
   return (
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,3 +1,33 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Navigation, EffectFade } from "swiper/modules";
+import "swiper/css/navigation";
+import "swiper/css/effect-fade";
+import img2048 from "../../assets/projects/2048.png";
+import imgDoodleDo from "../../assets/projects/DoodleDo.png";
+import "./Projects.css";
+
 export default function Projects() {
-  return <p>Projects</p>;
+  return (
+    <>
+      <div className="container"></div>
+
+      <Swiper
+        modules={[Navigation, EffectFade]}
+        navigation
+        effect="fade"
+        speed={800}
+        slidesPerView={1}
+        loop={true}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src={img2048} alt="2048" className="carousel-image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={imgDoodleDo} alt="DoodleDo" className="carousel-image" />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
 }
